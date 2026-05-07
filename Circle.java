f0c67426f73952dfd752bff7c4700fc29ad0ab00
EdSilverman <edwardsilverman517@gmail.com>
2026-05-07 02:01:05 -0400

    Updated: Circle.java

--- Circle.java
+++ Circle.java
@@ -20,26 +20,26 @@ class Circle extends Shape {
     }
 
     @Override 
-    public void draw(Turtle t){
-        double circumference = 2 * Math.PI * radius;
-        double sideLength = circumference / 365.0;
+public void draw(Turtle t){
+    double circumference = 2 * Math.PI * radius;
+    double sideLength = circumference / 45.0; // 90 steps instead of 360
 
-        t.penColor(this.getColor());
-        t.up();
-        t.forward(radius);
-        t.left(90);
-        t.down();
+    t.penColor(this.getColor());
+    t.up();
+    t.forward(radius);
+    t.left(90);
+    t.down();
 
-     for (int i = 0; i < 360; i++) {
-            t.forward(sideLength);
-            t.left(1);
-        }
-
-        t.up();
-        t.left(90);
-        t.forward(-radius);
-        t.down();
+    for (int i = 0; i < 45; i++) {
+        t.forward(sideLength);
+        t.left(8); // Turn 4 degrees per step (360 / 90 = 4)
     }
+
+    t.up();
+    t.left(90);
+    t.forward(-radius);
+    t.down();
+}
 
     @Override
     public void draw(Turtle t, double x, double y) {
