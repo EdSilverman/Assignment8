af316d18515318a75d00b226821808e038c1d4d9
TDudeTheTrain <tguzz@me.com>
2026-05-07 00:46:28 -0400

    Update: App.java

--- App.java
+++ App.java
@@ -1,17 +1,52 @@
+import java.util.Scanner;
+
 public class App {
 
     //Main is being used to test the stuff for now. Dont worry about implimenting a main function now. 
     
      public static void main(String[] args ){
+        
+        Scanner scanner = new Scanner(System.in);
 
-    Turtle t = new Turtle();
+        Turtle t = new Turtle();
+        String userChoice1; 
+        String userChoice2;
+
+
+        do {
+            System.out.println("Choose one of the options below");
+            System.out.println("1. Draw a Shape");
+            System.out.println("2. Draw a shape at a specific point");
+            System.out.println("0. Exit");
+
+            userChoice1 = scanner.nextLine();
+            
+            if (userChoice1.equals("1")){
+                System.out.println("Choose a shape to draw");
+
+                System.out.println("1. Circle");
+                System.out.println("2. Rectangle");
+                System.out.println("3. Triangle");
+                System.out.println("4. Pentagon");
+
+
+            } else if (userChoice1.equals("2")){
+                System.out.println("Choose a shape to draw at a specific point");
+
+                System.out.println("1. Circle");
+                System.out.println("2. Rectangle");
+                System.out.println("3. Triangle");
+                System.out.println("4. Pentagon");
+
+                
+            }
+
+
+
+
+        } while (!userChoice1.equals("0"));
     
-    Rectangle rectangle = new Rectangle(50, 60, "red");
-    Circle circle = new Circle(25, "blue");
-
-    rectangle.draw(t, -30, 20);
-    circle.draw(t, -80, -40);
-    rectangle.setShapeColor(t);
+    
     }
 
 }
