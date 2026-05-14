e6fb3895967279e38dd91f4b7115438991277809
EdSilverman <edwardsilverman517@gmail.com>
2026-05-14 02:20:47 -0400

    Update: Circle.java

--- Circle.java
+++ Circle.java
@@ -1,24 +1,52 @@
-class Circle extends Shape {  
+/**
+ * Represents a circle using the Turtle class. 
+ * The circle stores its radius and color, calculates its area, and can draw itself at a given location. 
+ * 
+ * @author Edward Silverman
+ */class Circle extends Shape {  
     private double radius;
 
+    /**
+     * Constructs a Circle object with the specified radius and color. 
+     *
+     * @param radius the radius of the circle 
+     * @param color the color of the circle 
+     */
     public Circle(double radius, String color){
         super(color, 0);
         this.radius = radius;
 
     }
 
+    /**
+     * Calculates and returns the area of the circle. 
+     *
+     * @return the area of the circle 
+     */
     @Override 
     public double getArea(){
         return Math.PI * radius * radius;
 
     }
 
+    /**
+     * Sets the Turtle pen color to the specified color. 
+     *
+     * @param t the Turtle object used for drawing 
+     * @param color the color to set for the Turtle pen 
+     */
     @Override 
     public void setShapeColor(Turtle t, String color){
         t.penColor(color);
 
     }
 
+    /**
+     * Draws the circle using the Turtle object. 
+     * The circle is approximated using 45 line segments. 
+     *
+     * @param t the Turtle object used for drawing 
+     */
     @Override 
 public void draw(Turtle t){
     double circumference = 2 * Math.PI * radius;
@@ -41,6 +69,13 @@ public void draw(Turtle t){
     t.down();
 }
 
+    /**
+     * Draws the circle at a specified position. 
+     *
+     * @param t the Turtle object used for drawing 
+     * @param x the x-coordinate of the starting position 
+     * @param y the y-coordinate of the starting position 
+     */
     @Override
     public void draw(Turtle t, double x, double y) {
         t.up();
@@ -52,6 +87,25 @@ public void draw(Turtle t){
         
     }
 
+    /**
+     * Returns a string representation of the Circle object. 
+     *
+     * @return a string containing the circle radius and color 
+     */
+    @Override
+    public String toString() {
+        return "Radius: " + radius + " Color: " + getColor();
+    }
+
+
+    /**
+     * Compares this Circle object with another object for equality. 
+     * Two circles are considered equal if they have the same radius 
+     * and the same color. 
+     *
+     * @param other the object to compare with 
+     * @return true if the objects are equal, false otherwise 
+     */
       @Override
     public boolean equals(Object other){
         if (other == null){
